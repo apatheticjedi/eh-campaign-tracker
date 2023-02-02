6d8bc1752d507c7bd120daa8fcd567a473730b96
Duder <thedudeabides1981@hotmail.com>
2023-02-01 17:01:18 -0800

    created models

---
+++ server/models/Campaign.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+
+// Schema for campaigns that includes comments
+const campaignSchema = new Schema(
+    {
+        scenarios: {
+            type: String,
+            required: true
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => dateFormat(timestamp)
+        },
+        status: {
+            type: String
+        },
+        investigators: [
+            {
+                type: String
+            }
+        ],
+        investStatus: {
+            type: String,
+        },
+        cities: {
+            type: String
+        },
+        notes: {
+            type: String
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+const Campaign = model('Campaign', campaignSchema);
+
+module.exports = Campaign;
---
+++ server/models/index.js
@@ -0,0 +1,5 @@
+const User = require('./User');
+const Campaign = require('./Campaign');
+
+
+module.exports = { User, Campaign };
